8464314fb7b57fdf979695f7c911e59e2a1a7f1d
NewbieAnderson <engineeringsong@gmail.com>
2023-04-08 23:49:18 +0900

    POC code for stack overflow

---
+++ stack_overflow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+void foo(void)
+{
+    printf("How did you execute this?\n");
+}
+
+int main(int argc, char **argv)
+{
+    char buf[20];
+    if (argc != 2)
+        printf("must one argument needed!\n");
+    printf("passed message : %s\n", argv[1]);
+    strcpy(buf, argv[1]);
+    return 0;
+}
